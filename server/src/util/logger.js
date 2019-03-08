4593e1fca29a9a4b1af6be2131c3fb6090e0247f
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2019-03-08 00:34:08 -0300

    fix: fix error on wiston logger

--- server/src/util/logger.js
+++ server/src/util/logger.js
@@ -1,18 +1,20 @@
 const winston = require("winston");
 
-const logger = new winston.Logger({
+const logger = winston.createLogger({
   transports: [
     new winston.transports.Console({
       level: process.env.NODE_ENV === "production" ? "info" : "debug",
-      colorize: true,
-      timestamp: true,
-      prettyPrint: true,
+      format: winston.format.json(),
+      defaultMeta: { service: 'user-service' },
+      // colorize: true,
+      // timestamp: true,
+      // prettyPrint: true,
       label: "experts-server"
     })
   ]
 });
 
-module.exports = logger
+module.exports = logger;
 
 // create stream for morgan
 logger.stream = {
