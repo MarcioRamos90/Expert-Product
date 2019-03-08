73f70296d8678a7acbdbfa108d984dbb3c2f341b
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2019-03-08 00:09:26 -0300

    feat: created logger file

---
+++ server/src/util/logger.js
@@ -0,0 +1,20 @@
+const winston = require("winston");
+
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      level: process.env.NODE_ENV === "production" ? "info" : "debug",
+      colorize: true,
+      timestamp: true,
+      prettyPrint: true,
+      label: "experts-server"
+    })
+  ]
+});
+
+module.exports = logger
+
+// create stream for morgan
+logger.stream = {
+  write: message => logger.info(message)
+};
